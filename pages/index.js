9401e872787b837a4cace05fc142a05207116e0f
clarajeon126 <clarajeon126@gmail.com>
2022-07-16 18:10:15 -0400

    changed so only one selected has no issue

--- pages/index.js
+++ pages/index.js
@@ -523,7 +523,7 @@ export default function Home() {
 
         {step !== 3 ? 
         (
-          <Arrangement vaseNum={vase} chosenFlowerArr={[foliage + 1, fillers[0] + 1, fillers[1] + 1, focal + 1]}/>
+          <Arrangement vaseNum={vase} chosenFlowerArr={(fillers.length >= 2) ? [foliage + 1, fillers[0] + 1, fillers[1] + 1, focal + 1] : [foliage + 1, fillers[0] + 1, fillers[0] + 1, focal + 1]}/>
         ) : (<Card Gifter={noteGifter} Recipient={noteRecipient} Content={noteContent} Preview={"true"}></Card>)}
           </div>
       </main>
