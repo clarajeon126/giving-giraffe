b11e729b9b5ffc35330d099809a56590f6f2bf5a
clarajeon126 <clarajeon126@gmail.com>
2022-07-17 00:07:05 -0400

    letter changed

--- components/Card.js
+++ components/Card.js
@@ -1,36 +1,31 @@
+import Image from 'next/image'
 import React, { Suspense, useEffect, useState } from 'react'
 import styles from '../styles/recipient.module.css'
-
+import card from '../public/card.png'
 export const Card = (props) => {
   console.log(props.Preview)
   const PreviewTest = props.Preview
   return (
-    <div>
-      {PreviewTest ?
-      (<div className={styles.cardpreview}>
-        
-      <div className={styles.leftpanel}>
+    <div className={styles.centered}>
+        <div className={styles.cardImg}>
+          {PreviewTest ?
+          (<div className={styles.rightpanel}>
+              <p className={styles.textWrap}>To: {atob(props.Recipient)}</p>
+              <p className={styles.textWrap}>{atob(props.Content)}</p>
+              <p className={styles.textWrap}>From: {atob(props.Gifter)}</p>
+            </div>
+            ) :
+            (<div className={styles.rightpanel}>
+              <p className={styles.textWrap}>To: {props.Recipient}</p>
+              <p className={styles.textWrap}>{props.Content}</p>
+              <p className={styles.textWrap}>From: {props.Gifter}</p>
+            </div>
+          )
+        }
+        </div>
+    </div>
 
-      </div>
-      <div className={styles.rightpanel}>
-      <p>To: {atob(props.Recipient)}</p>
-      <p>{atob(props.Content)}</p>
-      <p>From: {atob(props.Gifter)}</p>
-      </div>
-      </div>)
-       : (
-      <div className={styles.card}>
-      <div className={styles.leftpanel}>
-      </div>
-      <div className={styles.rightpanel}>
-      <p>To: {props.Recipient}</p>
-      <p>{props.Content}</p>
-      <p>From: {props.Gifter}</p>
-      </div>
-      </div>
-      ) }
 
-      </div>
   )
 }
 
